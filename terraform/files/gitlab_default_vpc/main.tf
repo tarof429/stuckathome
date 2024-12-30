90dc0a5b00223769fd70362079f64ee214f3431d
Taro Fukunaga <tarof429@gmail.com>
2024-12-30 17:54:58 +0000

    Fix security group definition for gitlab

--- terraform/files/gitlab_default_vpc/main.tf
+++ terraform/files/gitlab_default_vpc/main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "gitlab_sg" {
 resource "aws_vpc_security_group_egress_rule" "gitlab_sg" {
   security_group_id = aws_security_group.gitlab_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 0
+  from_port         = -1
+  to_port           = -1
   ip_protocol       = -1
 }
